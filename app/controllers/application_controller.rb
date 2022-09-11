4603e55bdfcb40abb681cb53a35e68f377355bb2
Eugene <eugeneayonga@gmail.com>
2022-09-11 09:01:08 +0300

    Built GET route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
+
+  get "/books" do
+    books = Book.all
+    books.to_json(include: :reviews)
   end
 
 end
