c1f0fd026dbc48bbb45e0beff88ef5e212863be0
Eugene <eugeneayonga@gmail.com>
2022-09-11 09:07:07 +0300

    Built a PATCH route for likes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,4 +32,10 @@ class ApplicationController < Sinatra::Base
     new_review.to_json(include: :book)
   end
 
+  patch '/books/:id' do 
+    book = Book.find_by(id: params[:id])
+    book.update(likes: params[:likes])
+    book.to_json(include: :reviews)
+  end
+
 end
