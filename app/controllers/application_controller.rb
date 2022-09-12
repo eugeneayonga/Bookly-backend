8533d6fe114f34eb08f7d776d758cb72794a18a1
Eugene <eugeneayonga@gmail.com>
2022-09-12 15:23:48 +0300

    Code clean-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  # get "/" do
-  #   { message: "Good luck with your project!" }.to_json
-  # end
-
-  # GET routes
   get "/books" do
     books = Book.all
     books.to_json(include: :reviews)
